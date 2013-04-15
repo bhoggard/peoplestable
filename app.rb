99589747cb8d1d06874b5ace3b4686b031a1556a
Barry Hoggard <barry@tristanmedia.com>
2013-04-15 18:59:29 -0400

    don't put hash in the hashtag in env var, seems to give heroku client hiccups

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ require "twitter_search_photos"
 
 # define a route that uses the helper
 get '/twitter' do
-  response = TwitterSearchPhotos.search(ENV['SEARCH_TAG'])
+  response = TwitterSearchPhotos.search('#' + ENV['SEARCH_TAG'])
   results = []
   response.results.each do |result|
     results << { user: result.screen_name, thumb_url: "#{result.media_url}:thumb", display_url: result.display_url }

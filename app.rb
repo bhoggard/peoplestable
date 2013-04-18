0953975a1ab016a582afeac0869513e589e7af25
Barry Hoggard <barry@tristanmedia.com>
2013-04-17 21:42:53 -0400

    more stacking experiments

--- app.rb
+++ app.rb
@@ -4,13 +4,7 @@ get '/' do
   redirect '/index.html'
 end
 
-# define a route that uses the helper
 get '/twitter' do
-  response = TwitterSearchPhotos.search('#' + ENV['SEARCH_TAG'])
-  results = []
-  response.results.each do |result|
-    results << { user: result.screen_name, thumb_url: "#{result.media_url}:thumb", display_url: result.display_url }
-  end
-  json max_id: response.max_id, results: results
+  json (Photo.all.to_a * 100)
 end
 
--- public/application.js
+++ public/application.js
@@ -1,3 +1,20 @@
-$.getJSON('/twitter', function(data) {
-  // console.log(data);
-});
+$(
+  $.getJSON('/twitter', function(data) {
+    photos = $('#photos');
+    $.each(data, function(index, value) {
+      img        = $('<img />', { src: value.thumb_url, alt: value.screen_name });
+      link       = $('<a />', { href: value.link_url, html: img });
+      bottom     = 53 * Math.floor(index / 5)
+      thumb_div  = $('<div />', { class: 'thumb thumb' + (index % 5).toString(), html: link })
+        .css("bottom", bottom);
+      if (bottom > 200 && bottom < 300) {
+        photos.height(bottom)
+      }
+      photos.prepend(thumb_div);
+    });
+  })
+);
+
+
+
+

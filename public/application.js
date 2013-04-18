e6c97724afed03c9101014415df75c39444cb1ef
Barry Hoggard <barry@tristanmedia.com>
2013-04-18 19:48:39 -0400

    Modularize the call to add a photo div

--- public/application.js
+++ public/application.js
@@ -1,30 +1,31 @@
+var PeoplesTable = {}
+PeoplesTable.counter = 0
+PeoplesTable.delay_time = 0
+
+PeoplesTable.image_div = function(image_data) {
+  img    = $('<img />', { src: image_data.thumb_url, alt: image_data.screen_name })
+    .slideDown('slow').hide().delay(PeoplesTable.delay_time).fadeIn('slow').rotate(Math.floor(15 - Math.random() * 30));
+  link   = $('<a />', { target: '_blank', href: image_data.link_url, html: img });
+  bottom = 53 * Math.floor(PeoplesTable.counter / 5);
+  // if we're full, randomly place it
+  if (PeoplesTable.counter > 54) {
+    bottom = 53 * (Math.floor(1 + Math.random() * 8))
+  }
+  PeoplesTable.counter += 1;
+  PeoplesTable.delay_time += 50;
+
+  return $('<div />', { class: 'thumb', html: link })
+    .css("bottom", bottom + Math.floor(1 + Math.random() * 8))
+    .css("right", Math.floor(1 + Math.random() * 8) + (PeoplesTable.counter % 5) * 53)
+    .css("z-index", Math.floor(1 + Math.random() * 20));
+}
+
 $(
   $.getJSON('/twitter', function(data) {
     photos = $('#photos');
-
-    var eT = 0;
-
     $.each(data, function(index, value) {
-      img_id = "image_" + index
-      img    = $('<img />', { id: img_id, src: value.thumb_url, alt: value.screen_name }).slideDown('slow').hide().delay(eT).fadeIn('slow').rotate(Math.floor(15 - Math.random() * 30));
-      link   = $('<a />', { target: '_blank', href: value.link_url, html: img });
-      bottom = 53 * Math.floor(index / 5);
-
-      // if we're full, randomly place it
-      if (index > 54) {
-        bottom = 53 * (Math.floor(1 + Math.random() * 8))
-      }
-
-      thumb_div = $('<div />', { class: 'thumb', html: link })
-        .css("bottom", bottom + Math.floor(1 + Math.random() * 8))
-        .css("right", Math.floor(1 + Math.random() * 8) + (index % 5) * 53)
-        .css("z-index", Math.floor(1 + Math.random() * 20));
-      photos.prepend(thumb_div);
-      eT += 50;
+      photos.prepend(PeoplesTable.image_div(value));
     });
   })
 );
-
-
-
 

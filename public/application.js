e84ddb694a9d31e8588a58f821bbf187fa030f98
Barry Hoggard <barry@tristanmedia.com>
2013-04-18 14:53:48 -0400

    more rows of images, added a slidedown effect

--- public/application.js
+++ public/application.js
@@ -1,14 +1,17 @@
 $(
   $.getJSON('/twitter', function(data) {
     photos = $('#photos');
+
+    var eT = 0;
+
     $.each(data, function(index, value) {
       img_id = "image_" + index
-      img    = $('<img />', { id: img_id, src: value.thumb_url, alt: value.screen_name }).rotate(Math.floor(15 - Math.random() * 30));
+      img    = $('<img />', { id: img_id, class: 'image', src: value.thumb_url, alt: value.screen_name }).slideDown('slow').hide().delay(eT).fadeIn('slow').rotate(Math.floor(15 - Math.random() * 30));
       link   = $('<a />', { href: value.link_url, html: img });
       bottom = 53 * Math.floor(index / 5);
 
       // if we're full, randomly place it
-      if (index > 44) {
+      if (index > 54) {
         bottom = 53 * (Math.floor(1 + Math.random() * 8))
       }
 
@@ -17,6 +20,7 @@ $(
         .css("right", Math.floor(1 + Math.random() * 8) + (index % 5) * 53)
         .css("z-index", Math.floor(1 + Math.random() * 20));
       photos.prepend(thumb_div);
+      eT += 50;
     });
   })
 );

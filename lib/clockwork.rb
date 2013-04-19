0df1e598b317c873528a9d45610fdac887af001b
Barry Hoggard <barry@tristanmedia.com>
2013-04-19 14:38:28 -0400

    remove unused clockwork file

--- lib/clockwork.rb
+++
@@ -1,4 +0,0 @@
-include Clockwork
-
-every(4.minutes, 'Queueing interval job') { Delayed::Job.enqueue IntervalJob.new }
-every(1.day, 'Queueing scheduled job', :at => '14:17') { Delayed::Job.enqueue ScheduledJob.new }

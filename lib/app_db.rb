625cc10654b1a0117e9291d36351240d35e30b2e
Barry Hoggard <barry@tristanmedia.com>
2013-04-19 11:26:57 -0400

    db setup typo fix

--- lib/app_db.rb
+++ lib/app_db.rb
@@ -5,7 +5,7 @@ module AppDB
     if ENV['MONGOHQ_URL']
       db_uri = URI.parse(ENV['MONGOHQ_URL'])
       db_name = db_uri.path.gsub(/^\//, '')
-      db = Mongo::Connection.new(db.host, db.port).db(db_name)
+      db = Mongo::Connection.new(db_uri.host, db_uri.port).db(db_name)
       db.authenticate(db_uri.user, db_uri.password) unless (db_uri.user.nil? || db_uri.user.nil?)
       db
     else
